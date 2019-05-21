6f4d62d87bfd59bac4f2fea9813a3ae033872cc3
kbrown1504 <keegqbrown@gmail.com>
2019-05-21 13:56:13 -0500

    step one done

---
+++ MCMS/MagicalCreature.swift
@@ -0,0 +1,19 @@
+//
+//  MagicalCreature.swift
+//  MCMS
+//
+//  Created by Keegan Brown on 5/21/19.
+//  Copyright © 2019 Keegan Brown. All rights reserved.
+//
+
+import Foundation
+
+class MagicalCreature {
+    
+    var name: String!
+    
+    init (name: String){
+        self.name = name
+    }
+    
+}
--- MCMS/ViewController.swift
+++ MCMS/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    var magicalCreatureArray: [MagicalCreature] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let creature1 = MagicalCreature(name: "Smaug")
+        let creature2 = MagicalCreature(name: "Troll Under the Bridge")
+        let creature3 = MagicalCreature(name: "Shrek")
+        magicalCreatureArray = [creature1, creature2, creature3]
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return magicalCreatureArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = mainTableView.dequeueReusableCell(withIdentifier: "CellID") as! UITableViewCell
+        cell.textLabel?.text = magicalCreatureArray[indexPath.row].name!
+        
+        return cell
+        
+    }
 
 }
 
